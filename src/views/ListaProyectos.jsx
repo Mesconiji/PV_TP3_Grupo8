3911246ab28b922935e55853bfdd461fb8907fed
Santiago Urzagaste <urzagastesantiago95@gmail.com>
2026-06-01 19:08:28 -0300

    2da Correccion de escritura en importacion Santiago Urzagaste Listaproyectos.jsx

--- src/views/ListaProyectos.jsx
+++ src/views/ListaProyectos.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
-import { Box , Typhograpy , TextField , Container } from '@mui/material'
+import { Box , Typography , TextField , Container } from '@mui/material'
 import proyectoService from '../services/proyectoService'
 import ProyectoCard from '../components/ProyectoCard'
 import DetalleProyecto from './DetalleProyecto'
